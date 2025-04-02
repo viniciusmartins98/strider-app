6e6129969b7c7ecb6e94fc755b1f7f7c1a09deae
Vinícius <vinicius.mp.silva@outlook.com>
2025-04-02 20:28:21 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import RouterComponent from "./routes";
+
+function App() {
+  return <RouterComponent />;
+}
+
+export default App;
---
+++ src/pages/LoginPageComponent.tsx
@@ -0,0 +1,3 @@
+export default function LoginPageComponent() {
+  return <h1>Login</h1>;
+}
