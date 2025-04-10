2e27d77104efe8f0ce9c3d49edcc358060d30f09
Vinícius <vinicius.mp.silva@outlook.com>
2025-04-09 22:22:43 -0300

    Added env files

--- src/pages/HomePageComponent.tsx
+++ src/pages/HomePageComponent.tsx
@@ -1,3 +1,10 @@
+import config from "../config";
+
 export default function HomePageComponent() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>Home</h1>
+      <h2>Url API:</h2> <span>{config.apiBaseUrl}</span>
+    </>
+  );
 }
